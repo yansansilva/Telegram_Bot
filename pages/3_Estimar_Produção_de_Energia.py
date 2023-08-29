d00eba0c3a95986d93b57946834131be07df7295
yansansilva <86764284+yansansilva@users.noreply.github.com>
2023-08-29 17:21:55 -0300

    Update 3_Estimar_Produção_de_Energia.py

--- pages/3_Estimar_Produção_de_Energia.py
+++ pages/3_Estimar_Produção_de_Energia.py
@@ -60,10 +60,9 @@ with tabs[0]:
         arquivo_ambiente = coluna_upload_3.file_uploader('Dados do Ambiente', type=['CSV'])
     else:
 	    dados_modulo, dados_inversor, dados_ambiente = import_from_GoogleDrive()
-	    st.write(dados_ambiente)
 	    dadosAmbienteValidos = dados_ambiente[(dados_ambiente.dropna().values != 0).all(axis=1)]
 	    st.write(dadosAmbienteValidos)
-	    dadosAmbienteValidos['Data'] = pd.to_datetime(dadosAmbienteValidos['Data'], dayfirst=False)
+	    dadosAmbienteValidos['Data'] = pd.to_datetime(dadosAmbienteValidos['Data'])
 	    Iinci = dadosAmbienteValidos['Gk'].values  # Cria um vetor irradiância Iinci, eliminando os valores nulos
 	    Tambi = dadosAmbienteValidos['Ta'].values  # Cria um vetor temperatura ambiente Tamb, eliminando os valores
 	    # correspondentes ao zero de irradiância
