214c44f2d5056413588f206dfd588cbf15bf7d6b
yansansilva <86764284+yansansilva@users.noreply.github.com>
2023-08-30 10:53:20 -0300

    Update 3_Estimar_Produção_de_Energia.py

--- pages/3_Estimar_Produção_de_Energia.py
+++ pages/3_Estimar_Produção_de_Energia.py
@@ -94,6 +94,7 @@ with tabs[1]:
             dadosAmbienteValidos = dados_ambiente[(dados_ambiente.values != 0).all(axis=1)]
             st.write(dadosAmbienteValidos)
             dadosAmbienteValidos['Data'] = pd.to_datetime(dadosAmbienteValidos['Data'], dayfirst=True)
+            st.write(dadosAmbienteValidos)
             Iinci = dadosAmbienteValidos['Gk'].values  # Cria um vetor irradiância Iinci, eliminando os valores nulos
             Tambi = dadosAmbienteValidos['Ta'].values  # Cria um vetor temperatura ambiente Tamb, eliminando os valores
             # correspondentes ao zero de irradiância
