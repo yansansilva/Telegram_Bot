912c9b5ac17ce95de95b552514df7c23b2578b60
yansansilva <86764284+yansansilva@users.noreply.github.com>
2023-08-30 09:41:45 -0300

    Update 5_Geração_e_Consumo_da_Edificação.py

--- pages/5_Geração_e_Consumo_da_Edificação.py
+++ pages/5_Geração_e_Consumo_da_Edificação.py
@@ -1,3 +1,6 @@
+import datetime
+import time
+
 import streamlit as st
 import gspread
 from google.oauth2.service_account import Credentials
@@ -9,7 +12,7 @@ st.set_page_config(
     page_icon="👋",
     layout="wide"
 )
-  
+
 lista_nomes_arquivos_climatizacao = Access_Folder()[0]['name']
 lista_nomes_arquivos_equipamentos = Access_Folder()[1]['name']
 lista_nomes_arquivos_iluminacao = Access_Folder()[2]['name']
@@ -20,43 +23,9 @@ def clear_cache():
     from streamlit.runtime.caching import cache_data_api
     cache_data_api.CachedFunc.clear(import_from_GoogleSheets)
 
-st.sidebar.button("Atualizar Dados",on_click=clear_cache)
+st.sidebar.button("Atualizar Dados", on_click=clear_cache)
 
 dados = import_from_GoogleSheets(lista_nomes_arquivos_teste)
-
-incluir_demanda_ativa = st.checkbox('Calcular/Incluir Demanda Ativa')
-if incluir_demanda_ativa:
-    dados[0]['Demanda Ativa A'], dados[0]['Demanda Ativa B'], dados[0]['Demanda Ativa C'] = dados[0]['Potência Ativa A'] * 5/60, dados[0]['Potência Ativa B'] * 5/60, dados[0]['Potência Ativa C'] * 5/60
-    dados[1]['Demanda Ativa A'], dados[1]['Demanda Ativa B'], dados[1]['Demanda Ativa C'] = dados[1]['Potência Ativa A'] * 5/60, dados[1]['Potência Ativa B'] * 5/60, dados[1]['Potência Ativa C'] * 5/60
-    dados[2]['Demanda Ativa A'], dados[2]['Demanda Ativa B'], dados[2]['Demanda Ativa C'] = dados[2]['Potência Ativa A'] * 5/60, dados[2]['Potência Ativa B'] * 5/60, dados[2]['Potência Ativa C'] * 5/60
-    dados[3]['Demanda Ativa A'], dados[3]['Demanda Ativa B'], dados[3]['Demanda Ativa C'] = dados[3]['Potência Ativa A'] * 5/60, dados[3]['Potência Ativa B'] * 5/60, dados[3]['Potência Ativa C'] * 5/60
-    dados[4]['Demanda Ativa A'], dados[4]['Demanda Ativa B'], dados[4]['Demanda Ativa C'] = dados[4]['Potência Ativa A'] * 5/60, dados[4]['Potência Ativa B'] * 5/60, dados[4]['Potência Ativa C'] * 5/60
-    dados[5]['Demanda Ativa A'], dados[5]['Demanda Ativa B'], dados[5]['Demanda Ativa C'] = dados[5]['Potência Ativa A'] * 5/60, dados[5]['Potência Ativa B'] * 5/60, dados[5]['Potência Ativa C'] * 5/60
-    dados[6]['Demanda Ativa A'], dados[6]['Demanda Ativa B'], dados[6]['Demanda Ativa C'] = dados[6]['Potência Ativa A'] * 5/60, dados[6]['Potência Ativa B'] * 5/60, dados[6]['Potência Ativa C'] * 5/60
-    
-#if st.checkbox('Calcular/Incluir Energia Ativa'):
-#    if incluir_demanda_ativa:
-#        dados['Demanda Ativa trifásica'] = (dados['Potência Ativa A'] + dados['Potência Ativa B'] + dados['Potência Ativa C']) * 5/60
-#    else:
-        
-if st.checkbox('Calcular/Incluir Potência Ativa Trifásica'):
-    dados[0]['Potência Ativa trifásica'] = dados[0]['Potência Ativa A'] + dados[0]['Potência Ativa B'] + dados[0]['Potência Ativa C'] * 5/60
-    dados[1]['Potência Ativa trifásica'] = dados[1]['Potência Ativa A'] + dados[1]['Potência Ativa B'] + dados[1]['Potência Ativa C'] * 5/60
-    dados[2]['Potência Ativa trifásica'] = dados[2]['Potência Ativa A'] + dados[2]['Potência Ativa B'] + dados[2]['Potência Ativa C'] * 5/60
-    dados[3]['Potência Ativa trifásica'] = dados[3]['Potência Ativa A'] + dados[3]['Potência Ativa B'] + dados[3]['Potência Ativa C'] * 5/60
-    dados[4]['Potência Ativa trifásica'] = dados[4]['Potência Ativa A'] + dados[4]['Potência Ativa B'] + dados[4]['Potência Ativa C'] * 5/60
-    dados[5]['Potência Ativa trifásica'] = dados[5]['Potência Ativa A'] + dados[5]['Potência Ativa B'] + dados[5]['Potência Ativa C'] * 5/60
-    dados[6]['Potência Ativa trifásica'] = dados[6]['Potência Ativa A'] + dados[6]['Potência Ativa B'] + dados[6]['Potência Ativa C'] * 5/60
-    
-if st.checkbox('Calcular/Incluir Demanda Ativa Trifásica'):
-    dados[0]['Demanda Ativa trifásica'] = (dados[0]['Potência Ativa A'] + dados[0]['Potência Ativa B'] + dados[0]['Potência Ativa C']) * 5/60
-    dados[1]['Demanda Ativa trifásica'] = (dados[1]['Potência Ativa A'] + dados[1]['Potência Ativa B'] + dados[1]['Potência Ativa C']) * 5/60
-    dados[2]['Demanda Ativa trifásica'] = (dados[2]['Potência Ativa A'] + dados[2]['Potência Ativa B'] + dados[2]['Potência Ativa C']) * 5/60
-    dados[3]['Demanda Ativa trifásica'] = (dados[3]['Potência Ativa A'] + dados[3]['Potência Ativa B'] + dados[3]['Potência Ativa C']) * 5/60
-    dados[4]['Demanda Ativa trifásica'] = (dados[4]['Potência Ativa A'] + dados[4]['Potência Ativa B'] + dados[4]['Potência Ativa C']) * 5/60
-    dados[5]['Demanda Ativa trifásica'] = (dados[5]['Potência Ativa A'] + dados[5]['Potência Ativa B'] + dados[5]['Potência Ativa C']) * 5/60
-    dados[6]['Demanda Ativa trifásica'] = (dados[6]['Potência Ativa A'] + dados[6]['Potência Ativa B'] + dados[6]['Potência Ativa C']) * 5/60
-    
 data = pd.to_datetime(dados[0]['Hora'])
 coluna_1, coluna_2, coluna_3 = st.columns(3)
 filtro_ano = coluna_1.selectbox('Ano:', options=data.dt.year.drop_duplicates(), index=(len(data.dt.year.drop_duplicates()))-1)
@@ -73,7 +42,7 @@ else:
 
 divisao_tela1, divisao_tela2, divisao_tela3 = st.columns((2.75, 0.5, 2.75))
 
-if parametros_eletricos != []: 
+if parametros_eletricos != []:
     divisao_tela1.plotly_chart(plot_graficos(parametros_eletricos, dados[0], lista_nomes_arquivos_teste[0], filtro_data))
     divisao_tela3.plotly_chart(plot_graficos(parametros_eletricos, dados[1], lista_nomes_arquivos_teste[1], filtro_data))
     divisao_tela1.plotly_chart(plot_graficos(parametros_eletricos, dados[2], lista_nomes_arquivos_teste[2], filtro_data))
